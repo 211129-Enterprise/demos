2788df773b0eb06649348a48fd1b8d5d5acb1ea9
Aaron <a-medlock@live.com>
2021-11-29 15:19:24 -0800

    My first java class via Spring Tool Suite

---
+++ 1-core-java/FirstJavaProject/src/com/revature/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+public class HelloWorld {
+
+	// ctrl + spacebar allows me to shortcut method signatures
+	public static void main(String[] args) {
+		
+		// sysout + ctrl + space
+		System.out.println("Hello World... from the IDE");
+	}
+}
