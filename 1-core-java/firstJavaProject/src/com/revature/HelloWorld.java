db48e7b0d6d481e28a88edfcd3955d54ab0c1d51
Kenny Phu <kphu@outlook.com>
2021-11-30 12:55:19 -0500

    merge with main

--- 1-core-java/firstJavaProject/src/com/revature/HelloWorld.java
+++ 1-core-java/firstJavaProject/src/com/revature/HelloWorld.java
@@ -1,99 +1,147 @@
 package com.revature;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.revature.garbagecollection.GarbageDriver;
+
 public class HelloWorld {
-
-	// ctrl + spacebar allows me to shortcut method signatures
-	public static void main(String[] args) {
-		
-		// sysout + ctrl + space
-		System.out.println("Hellow World...from the IDE");
-
-		
-		/**
-		 * * This is a multi-line
-		 * comment.
-		 * 
-		 * Java has 8 Primitive Types
-		 */
-		
-		// 8 bits = 1 byte
-		boolean isCool = true; //1 bit of memory (0 or a 1) that represents true or false values
-		byte reallySmallNumber = 127; // 8 bits (1 byte) of memory reserved - max = 127, min = -128
-		short shortNumber = 32432; // 16 bits of reserved memory
-		char letter = 'm'; // 16 bits of memory that represents a character or the numerical value of an ASCII character
-		
-		int standardNum = 10_000_000; // 32 bits of memory (4 bytes)
-		
-		float lessPreciseDouble = 93.2f; // 32 bits of memory
-		double decimal = 20.45; // 64 bits of reserved memory
-		
-		long bigNumber = 1234567L; // 64 bits of reserved memory
 	
-		/** 
+		/**
+	 * In any Java Program the main() method is the entry point from where the
+	 * compiler starts program execution.
+	 * 
+	 * @param args ... represents commands that could follow the class name when we
+	 *             call the "java" execution command to execute the bytecode.
+	 * 
+	 *             We will talk later about what it means to be "public", "static",
+	 *             and "void"....
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Testing output to the console...");
+
+		/**
+		 * A literal is a source code representation of a fixed value. They are
+		 * represented directly in the code without any computation.
+		 * 
+		 * Literals can be assigned to any primitive type variable. For example:
+		 */
+
+		// 8 bits = 1 byte
+		boolean isCool = true; // 1 bit represents true or false value (either 0 or 1) 
+		byte reallySmallNumber = 127; // 8 bits (1 byte) of reserved memory -> max value is 127, min value -128.
+		short shortNumber = 32767; // 16 (2 bytes) bits of reserved mem
+		char letter = 'm'; // 16 bits of memory that represent a character or an ASCII character's numeric
+							// value
+		int standardNum = 10_000_000; // 32 bits of reserve mem (4 bytes)
+		float lessPreciseDouble = 93.2f; // 32 bits of mem, allows you to make decimals, but it's-- less precise than a
+											// double
+		double decimal = 20.45; // 64 bits of mem (8 bytes)
+		long bigNumber = 12345678L; // 64 bits
+
+		/*
 		 * Casting
 		 * 
-		 * Type casting is used to convert object or variables of one type into another
+		 * Type casting is used to convert objects or variables of one type into another
 		 * 
-		 * Widening Casting (Implicit) -- Automatic type conversion - widening
-		 * a smaller value to a larger primitive type
+		 * Widening Casting (Implicit) -- Automatic Type Conversion Widening a smaller
+		 * primitive value to a bigger primitive type.
 		 * 
-		 * Narrow Casting (Explicit) -- Need for manual conversion - Narrowing a bigger primitive
-		 * value to a small primitive value.
+		 * Narrowing Casting (Explicit) -- Need Explicit Conversion with () Narrowing a
+		 * bigger primitive value to a small primitive value.
 		 */
-		
+
 		byte a = 40;
-		short b = a; // b == 40
-		long bigNum = a;
-		
-		// a double is larger than an int
+		short b = a; // b == 40. a short has more reserved memory than a byte, so we can easily convert a byte to a short.
+
+		// byte anotherByte = b; // This isn't IMPLICITLY converted! We need to use
+		// casting for this. More on that in a bit.
+		int c = b;
+		double d = c;
+	
+		// a double is larger than an integer
 		double dub = 900.9;
-		int myInt = (int) dub;
+		int myInt = (int) dub; // we are using NARROW Casting to transform a large datatype to a small one
 		
-		System.out.println("The narrow (explicit) casted dub to an int is " + myInt);
+		System.out.println("the double that has been explicitly cast to an int is now equal to " + myInt);
 		
-		// String is a class, not a primitive type
-		String name = "Spongebob"; // this is a String Literal
+		int x = 4;
+		int y = 10;
 		
+		System.out.println(x + y);
+		
+		// a String is NOT primitive data type. It is technically a class
+		String name = "Spongebob";
+		
+		System.out.println(name + " is " + (x + y * 100) + " years old");
+			
+		// Everything above that we've defined are Java Literals, which are simply hard-coded fixed values that we assign to a variable.		
 	
-		int x = 30;
-		int y = -10;
-	
-		//Later we will talk about the String API
-		System.out.println(name + " is " + (x % y) + " years old");
-	
-		/**
-		 * Wrapper Classes
+		/*
+		 * What are Wrapper Classes?
 		 * 
-		 * A Wrapper Class is the class name of the data type
+		 * Wrapper class is the class name of the data type. 
+		 * Essentially it is the name
+		 * of the object when stored in the Heap.
 		 * 
-		 * Why do we use Wrapper Classes?
+		 * Why do we use Wrapper classes?
 		 * 
-		 * Because there are Data Structures within Java (specifically the Collections Framework)
-		 * that only store Objects.
+		 * Because data-structures within the Collections Framework Only work with
+		 * Objects.
 		 * 
 		 * What is an Object?
 		 * 
-		 * An object is a reusable component consisting of properties and methods which
-		 * we can call throughout an application.
-		 * These methods & properties are defined within a class.
-		 * 
+		 * A Java Object is a self-contained component which consists of methods and
+		 * properties to make certain types of data useful. These methods/properties are
+		 * defined in a Class!
 		 */
 		
-		int min = Integer.MIN_VALUE;
-		System.out.println("The minimum value of an integer is equal to... " + min);
+		int min; // Variable Declaration =- declaring the type
+		min = Integer.MIN_VALUE; // static variables are available on a class level
 		
-		// Here I am converting a primitive datatype into an Integer Object
-		Integer obj1 = new Integer(20); //This is called AUTOBOXING
-		Integer obj2 = new Integer("20"); // because we used the new keyword we instantiated a new object
-		
-		// Are the above objects the same objects created in memory?
-		System.out.println("Are the above objects the same objects created in memory? " + (obj1 == obj2)); // == tests address in memory
-		
-		// are the 2 objects equal?
-		System.out.println("Are the above objects of equal value? " + (obj1.equals(obj2))); //== tests address in memory
+		System.out.println("The minimum on an int is " + min);
 		
 		
+		// Completely convert the primitive data type into an Integer Object! 
+		// (AUTOBOXING) prim -> Class type (object)
+		Integer obj1 = new Integer(20); // creates a totally new object 
+		Integer obj2 = new Integer("20"); // also creates a totally new object 
 		
+		// Question 1: do obj1 & obj 2 have the same value?
+		System.out.println(obj1.equals(obj2)); // alt + shift + x, j -> YES they have the same value
+		
+		// Question 2: do obj1 & obj2 point to the same object in the heap?
+		System.out.println("testing the ==" + (obj1 == obj2)); // FALSE!  the == checks for address in memory
+		
+		// Unboxing: object -> primitive type
+		
+		// Challenge: Convert the following Double Object to a primitive double:
+		Double dubObj = 15.45; // we are instantiating a Double object that has the value of the initialized primitve literal.
+		Double dubObj2 = new Double(76.123); 
+		// convert that to a double....
+		double dubPrimitive = dubObj; // implicit way of transofrming it
+		
+		double dubPrimitive2 = Double.valueOf(dubObj); // explicut
+		
+		dubObj2 = null;
+
+		// if I were to do something with
+		List<Double> dubList = new ArrayList<Double>();
+		
+		// ctrl + shift + o to auto import everything
+		dubList.add(dubObj);
+		dubList.add(dubObj2);
+		
+		/**
+		 * Wrapper Classes
+		 * Unboxing
+		 * Autoboxing
+		 * 
+		 * == vs. .equals();
+		 */
+		
+		System.out.println(GarbageDriver.numThatNeverChanges); // i can call it's public static variable
 	}
 
 }
--- 1-core-java/firstJavaProject/src/com/revature/models/Person.java
+++ 1-core-java/firstJavaProject/src/com/revature/models/Person.java
@@ -1,26 +1,29 @@
 package com.revature.models;
 
-import java.util.Objects;
-
-// Remember that our custom class inherits from the java.lang.Object class
+// Remmeber that our custom class inherits from the java.lang.Object class
 public class Person { // POJO = Plain Old Java Object
 
 	// properties (states)
-	
-	// Instance variables - properties specific to each Person object that's instantiated
+
+	// Instance variables - properties specific to each Person object that's
+	// instantiated
 	private String name;
 	private int age;
 	private double height;
 	private double weight;
-	
+
 	// constructors
-	public Person(String name) {
-		super(); // I'm calling the Object class (or direct parent class)
-		this.name = name; // this points to the specific object that I'm instantiating's NAME property
-		
+	
+	public Person() {
+		// this is a no-args constructor which allows me to  create a person withour massing in args
 	}
 	
-	// a FULLY initialized person
+	public Person(String name) {
+		super(); // I'm calling the Object class (or direct parent class)
+		this.name = name; // this points to the specific object that I'm instantiating's NAME property 
+	}
+	
+	// a FULLY initialize person
 	public Person(String name, int age, double height, double weight) {
 		super();
 		this.name = name;
@@ -29,16 +32,17 @@ public class Person { // POJO = Plain Old Java Object
 		this.weight = weight;
 	}
 
-	// method (behaviors)
-	
-	public void introduce() { // void is a non-access modifier - specifies the return type which is nothing - it just does something
-		System.out.println("Hello my name is " + name); // by default, we know that name refers to the instance variable of this object
+	// methods (behaviors)
+	public void introduce() { // void is a non-access modifier - specifies the return type which is nothing - it just does something 
+		System.out.println("Hello my name is " + name); 
+		// by default, we know that name refers to the instance variable of this object that's calling it
 	}
 	
-	// we must generate getters & setters so that we can change the private 
-	//	properties of objects that are created from this class
+
+	// we must generate getters & setters so that we can change the private
+	// properties of the objects that are created from this class
 	
-	// right click => source => generate getters and setters
+	// right click -> source -> generate getters & setters
 	
 	public String getName() {
 		return name;
@@ -64,32 +68,51 @@ public class Person { // POJO = Plain Old Java Object
 	public void setWeight(double weight) {
 		this.weight = weight;
 	}
-	
+
 	// we can override a method that we inherit from the Object class
-	@Override // overriding the toString() method from the Object class to properly reflect the specific instance variable 
+	@Override // overrideing the toString() method from the IObject class to rpoerly reflect the specific instanct varibale of a Perosn object
 	public String toString() {
 		return "Person [name=" + name + ", age=" + age + ", height=" + height + ", weight=" + weight + "]";
 	}
 
-//	@Override
-//	public int hashCode() {
-//		return Objects.hash(age, height, name, weight);
-//	}
-//
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		Person other = (Person) obj;
-//		return age == other.age && Double.doubleToLongBits(height) == Double.doubleToLongBits(other.height)
-//				&& Objects.equals(name, other.name)
-//				&& Double.doubleToLongBits(weight) == Double.doubleToLongBits(other.weight);
-//	}
-//	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		long temp;
+		temp = Double.doubleToLongBits(height);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		temp = Double.doubleToLongBits(weight);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (Double.doubleToLongBits(height) != Double.doubleToLongBits(other.height))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(weight) != Double.doubleToLongBits(other.weight))
+			return false;
+		return true;
+	}
+
 	
 	
+
 }
