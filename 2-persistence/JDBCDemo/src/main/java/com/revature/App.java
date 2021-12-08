ea8c21939fffc62ad37aa81d102e1b70bea2cb4b
Somers B Matthews <somersbmatthews@gmail.com>
2021-12-08 10:07:32 -0600

    added .gitignore

--- 2-persistence/JDBCDemo/src/main/java/com/revature/App.java
+++ 2-persistence/JDBCDemo/src/main/java/com/revature/App.java
@@ -1,8 +1,12 @@
 package com.revature;
 
+import com.revature.util.ConnectionUtil;
+
 public class App {
 	public static void main(String[] args) {
 		run();
+		
+		ConnectionUtil.getConnection();
 	}
 	
 	public static void run() {
--- 2-persistence/JDBCDemo/src/main/java/com/revature/util/ConnectionUtil.java
+++ 2-persistence/JDBCDemo/src/main/java/com/revature/util/ConnectionUtil.java
@@ -1,8 +1,12 @@
 package com.revature.util;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 // JDBC (Java Database Connectivity) API
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtil {
 	// singleton - cannot instantiate
@@ -24,13 +28,33 @@ public class ConnectionUtil {
 			return null;
 		}
 		
-		String url = "jbdc:postgresql://localhost:5432/postgres?currentSchema=somersm";
+		Properties prop = new Properties();
 		
+		String url = "";
+		String username = "";
+		String password = "";
 		try {
-			conn = DriverManager.getConnection(url, "postgres", "postgres");
+			// try this if it doesn't work C:\Users\somer\projects\demos\2-persistence\JDBCDemo\src\main\resources\application.properties
+			prop.load(new FileReader("C:\\Users\\somer\\projects\\demos\\2-persistence\\JDBCDemo\\src\\main\\resources\\application.properties"));
+			
+			url = prop.getProperty("url");
+			username = prop.getProperty("username");
+			password = prop.getProperty("password");
+			
+			conn = DriverManager.getConnection(url, username, password);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return null;
+		} catch (FileNotFoundException e) {
+			System.out.println("cannot locate properties file");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("something wrong with app.props");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		return conn;
 	}
 }
