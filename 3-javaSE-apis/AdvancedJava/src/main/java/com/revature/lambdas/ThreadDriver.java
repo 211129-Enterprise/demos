e920982b16daec81f81f0e0a013c220f89b074d5
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2021-12-13 15:33:54 -0500

    add all predicate notes

---
+++ 3-javaSE-apis/AdvancedJava/src/main/java/com/revature/lambdas/ThreadDriver.java
@@ -0,0 +1,52 @@
+package com.revature.lambdas;
+
+import java.util.List;
+import java.util.Vector;
+
+public class ThreadDriver {
+
+	public static void main(String[] args) {
+		
+		
+		List<Employee> employees = new Vector<>();
+		
+		// here we will instnatitae a thread that has the behavior of MyTask 
+		Thread t1 = new Thread(new MyTask());
+		
+		t1.start();
+		
+		// How would we do the same thing with an Anonymous class?
+		// Create a thread called with ref variable t2, and its run method says "Printing from the anonymous Runnable object "
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Printing from the Anonymous Runnable object!");
+				
+			}
+			
+		});
+		
+		t2.start();
+		
+		// how would you provide a method implementation for the run() method via a lambda expression
+		Thread  t3 = new Thread(() -> System.out.println("Running from the lambda expression implementation!"));		
+				
+		t3.start(); // "Running from the lambda expression implementation!
+
+	}
+
+}
+
+class MyTask implements Runnable { // functional interface implemented by 
+
+	@Override
+	public void run() {
+
+		System.out.println("Printing from the Runnable object, MyTask");
+		
+	}
+	
+	
+	
+}
