f0160f465d853abca32576e10e974b79e1175243
Somers B Matthews <somersbmatthews@gmail.com>
2021-12-13 11:19:17 -0600

    added class code and notes to week 3

---
+++ 3-JavaSE-apis/TheatreApp/src/main/java/com/revature/Theatre.java
@@ -0,0 +1,134 @@
+package com.revature;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.function.Consumer;
+// consumer, supplier, predicate
+
+
+public class Theatre {
+	private final String theatreName;
+	
+	
+	// SOPHIAQUESTION when changing arraylist to hashset, why does it not work?
+	private List<Seat> seats = new ArrayList<>();
+
+	public Theatre(String theatreName, int numRows, int seatsPerRow) {
+		this.theatreName = theatreName;
+		
+		int lastRow = 'A' + (numRows - 1);
+		
+		for (char row = 'A'; row <= lastRow; row++) {
+			for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
+			Seat seat = new Seat(row + String.format("%02d", seatNum));
+			seats.add(seat);
+			}
+		}
+	}
+	
+	// O(n) linear time complexity
+	public boolean researveSeatBruteForce(String seatNumber) {
+		// we will iterate through all the seats until we find the seat we're looking for
+		Seat requestedSeat = null;
+		
+		for(Seat seat : seats) {
+			System.out.println("searching...");
+			if(seat.getSeatNumber().equals(seatNumber)) {
+				// set the requestedSeat = to seat
+				requestedSeat = seat;
+				
+				break;
+			}
+		}
+		
+		if (requestedSeat == null) {
+			System.out.println("sorry, there is no seat " + seatNumber);
+			return false;
+		}
+		// and then we'll check if it's reserved
+		return requestedSeat.reserve();
+	}
+	
+
+
+	public void getSeats() {
+//		for (Seat s : seats) {
+//			System.out.println(s);
+//		}
+		
+		// first arguement is an abstract method called the accept method
+//		seats.forEach(seat -> {
+//			System.out.println(seat);
+//			System.out.println("Multiple lines of code!");
+//		});
+		
+		seats.forEach(seat -> System.out.println(seat));
+		
+		// lambdas have arg list, arrow, body
+		
+		// Method referencing
+//		seats.forEach(System.out::println);
+		
+		
+		
+		// ANONYMOUS CLASS
+		// CONSUMER interface is the only thing i need here
+		// I can implement its accept() of the anonymous class implementing consume interface
+		
+//		seats.forEach(new Consumer<Seat>() {
+//			@Override
+//			public void accept(Seat t) {
+//				System.out.println(t);
+//			}
+//		});
+		
+	}
+
+	public void setSeats(List<Seat> seats) {
+		this.seats = seats;
+	}
+
+	public String getTheatreName() {
+		return theatreName;
+	}
+
+	// nesting classes increases encapsulation
+	private class Seat {
+		private final String seatNumber;
+		private boolean reserved = false;
+		
+		public boolean reserve() {
+			if (!this.reserved) {
+				this.reserved = true;
+				System.out.println("Reservation of seat" + seatNumber + " confirmed.");
+				return true;
+			} else {
+				System.out.println("this seat is already reserved" + seatNumber);
+				return false; // otherwise it's already reserved
+			}
+		}
+		
+		
+
+		private Seat(String seatNumber) {
+			this.seatNumber = seatNumber;
+		}
+
+		public String getSeatNumber() {
+			return seatNumber;
+		}
+
+		public boolean isReserved() {
+			return reserved;
+		}
+
+		@Override
+		public String toString() {
+			return "Seat [seatNumber=" + seatNumber + ", reserved=" + reserved + "]";
+		}
+		
+		
+
+	}
+}
