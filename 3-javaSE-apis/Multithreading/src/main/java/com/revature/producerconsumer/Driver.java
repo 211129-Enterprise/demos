ed89e77ca8baee5bbd40031884f78bdde1eec494
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2021-12-14 12:00:25 -0500

    add initial thread notes

---
+++ 3-javaSE-apis/Multithreading/src/main/java/com/revature/producerconsumer/Driver.java
@@ -0,0 +1,25 @@
+package com.revature.producerconsumer;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		
+		Holder data = new Holder();
+		int numValuesToGenerate = 10;
+		
+		Thread producerThread = new Thread(new Producer(data, numValuesToGenerate)); // pass thru the producer runable object
+		Thread consumerThread  = new Thread(new Consumer(data, numValuesToGenerate)); // pass thru the consumer runnable object
+		
+		System.out.println("Starting threads");
+		
+		
+		producerThread.start();
+		consumerThread.start();
+		
+		
+		// introduce the join() method
+		
+		
+	}
+
+}
