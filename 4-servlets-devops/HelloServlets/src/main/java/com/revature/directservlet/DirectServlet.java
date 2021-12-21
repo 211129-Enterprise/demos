9cded31b8c5d3539a8c4573039b30e4c1d68e121
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2021-12-21 11:30:26 -0500

    add Hello servlets demo with CSS and error page

--- 4-servlets-devops/HelloServlets/src/main/java/com/revature/directservlet/DirectServlet.java
+++ 4-servlets-devops/HelloServlets/src/main/java/com/revature/directservlet/DirectServlet.java
@@ -8,11 +8,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.SuperVillain;
+
 public class DirectServlet extends HttpServlet {
 
 	/** When a client sends a GET request to the server at some location (which we have yet to map...this will be
 	 * done in our Deployment Descriptor - web.xml), it will trigger this method.
 	 */
+	
+	// init() is called 
+	
+	
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -29,4 +36,54 @@ public class DirectServlet extends HttpServlet {
 
 		// Now we need to map where this functionality is available
 	}
+	
+	/**
+	 * This method is responsible for handling a POST request that a client sends.
+	 * Instantiate an object, and send back the object's data in the form of JSON - JavaScript Object Notation
+	 * JSON = a data interchange format
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	
+		// Step 1. Declare the type of content for our HTTP response
+		response.setContentType("application/json");
+		
+		// Step 2. Instantiate an object
+		SuperVillain vill = new SuperVillain("The Penguin", "money", 900.10);
+		
+		// transform a java object into JSON and send it to the browser (client) in the body of the HTTP response
+		// Here we're both transforming is AND sending it via the print writer on the response
+		response.getWriter().write(new ObjectMapper().writeValueAsString(vill));
+		// import ObjectMapper from Jackson Databind
+	}
+	
+	/**
+	 * HTTP Response
+	 * 
+	 * Response Header
+	 * 	- HTTP Version
+	 * 	- Status code
+	 * 
+	 * Response Body (contains the data its sending back!)
+	 */
+	
+	
+	
+	// destroy() is called
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
