0c5adcb18ef38829ea742261595824196ff0b7f2
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-01-03 14:48:59 -0500

    complete DI example

--- 5-spring/SpringCore/src/main/java/com/revature/App.java
+++ 5-spring/SpringCore/src/main/java/com/revature/App.java
@@ -3,6 +3,7 @@ package com.revature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
@@ -11,15 +12,14 @@ public class App {
 	private static final Logger log = LoggerFactory.getLogger(App.class);
 	private static final String CONFIG_LOCAION = "beans.xml";
 	
-	
-	
-	
 	public static void main(String[] args) {
 		// The Spring IoC Container will manage Dependecy injection for us
 		
 		// Step 1. Create the Context (Application Context) by feeding it configuration details!
-		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCAION); // this reads from a config file
+//		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCAION); // this reads from a config file
 		// Bean wiring lives at that location
+		ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		// https://www.tutorialspoint.com/spring/spring_java_based_configuration.htm
 		
 		
 		// Step 2. Instantiate a NumberGenerator Bean using Spring IoC
@@ -37,11 +37,13 @@ public class App {
 		number = game.getNumber(); // fetches the new number generated after the reset
 		
 		log.info("The number is {}", number);
+		
+		MessageGenerator msg = context.getBean(MessageGenerator.class);
+		
+		log.info("getMainMessage = {}", msg.getMainMessage());
+		
 
 		context.close(); // prevent memory leakage
-		
-		
-
 	}
 
 }
--- 5-spring/SpringCore/src/main/java/com/revature/GameImpl.java
+++ 5-spring/SpringCore/src/main/java/com/revature/GameImpl.java
@@ -3,6 +3,7 @@ package com.revature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 public class GameImpl implements Game {
 	
