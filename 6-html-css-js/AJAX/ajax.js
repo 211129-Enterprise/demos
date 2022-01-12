0c9b2db33e18a66ee620b8acea742b5d14449d89
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-01-12 15:47:51 -0500

    add fetch and ajax notes

---
+++ 6-html-css-js/AJAX/ajax.js
@@ -0,0 +1,62 @@
+// grab the elements from the page to work with
+const pokeId = document.getElementById('poke-id')
+const respId = document.getElementById('resp-id')
+const pokeName = document.getElementById('resp-name')
+const pokeImg = document.getElementById('resp-sprite');
+const button = document.getElementById('btn')
+
+button.addEventListener('click', ajaxCall);
+
+// const ajaxCall = () => {
+
+// }
+
+function ajaxCall() {
+
+    // capture the id that hte user input
+    let idNum = pokeId.value;
+
+    // 1. attain the XMLHttpRequestObject
+    let xhr = new XMLHttpRequest();
+
+    /**
+     * 0. UNSENT: open() method has not been called 
+     * 1. OPENED: open() method has been called
+     * 2. HEADERS_REQUEST: send() method has been called and the ehaders + status code of the HTTP response are available
+     * 3. LOADING: downloading the full response from the server
+     * 4. DONE: entire operation is complete and now we have a server response.
+     */
+    xhr.onreadystatechange = function() {
+
+        // this inside of a function refers to the object that's calling the function
+        if (this.readyState == 4 && this.status == 200) {
+
+            // if we have successfully received the response we neeed to transform it to a JS Object
+            let respPokemon = JSON.parse(xhr.responseText);
+
+            console.log(respPokemon);
+
+            renderPokemon(respPokemon);
+        }
+
+    }
+
+    xhr.open("GET", `https://pokeapi.co/api/v2/pokemon/${idNum}`); // pokeapi.com
+
+    xhr.send();
+}
+
+function renderPokemon(data) {
+
+    // we're going to modify the elements on the index.html page
+    // by plugging in the values of the properties of the data recevievd
+
+    pokeName.innerHTML = `Name: ${data.name}` // /name and .id have to be properties on the JSON object
+    respId.innerHTML = `Id: ${data.id}`
+
+    pokeImg.setAttribute("src", data.sprites.front_default);
+
+    pokeImg.setAttribute("height", 300);
+
+}
+
