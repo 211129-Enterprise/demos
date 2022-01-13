ccf2f09ea08c265110a1c5fb755bc0a06032f34b
Mark Martin <95247709+MarkMartin00@users.noreply.github.com>
2022-01-13 13:08:27 -0600

    Thursday

---
+++ 7-Angular/functions.ts
@@ -0,0 +1,10 @@
+//tsc function.ts
+const myLog1 = function(message: string) {
+    console.log(message);
+}
+
+const myLog2 = (message: string) => {
+    console.log(message);
+}
+
+const myLog3 = (message: string) => console.log(message);
