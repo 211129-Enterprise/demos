41562c2b6c1d212df4df0917a5297758b5c02315
Mark Martin <95247709+MarkMartin00@users.noreply.github.com>
2022-01-14 09:48:57 -0600

    x

---
+++ 7-Angular/complete-api-ui/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { AllComponent } from './components/all/all.component';
+import { MainComponent } from './components/main/main.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {path: '', component: MainComponent},
+  {path: 'main', component: MainComponent}, // http:localhost:4200/main
+  {path: 'all', component: AllComponent},
+
+  {path: '**', component: MainComponent} // this is a WildCard Route to handle unknown paths (this MUST be last)
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
