ba13400d3e6c5637644404d7ae072bc1f0ea7316
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-01-18 07:18:22 -0500

    Update UserController.java

--- 5-spring/CompleteAPI/src/main/java/com/revature/controller/UserController.java
+++ 5-spring/CompleteAPI/src/main/java/com/revature/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.revature.model.User;
 import com.revature.service.UserService;
